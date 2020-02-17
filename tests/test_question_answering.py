de5498ceae8e54d0d269b11ee58c330a9aef0557
aychang95 <aychang995@gmail.com>
2020-02-17 06:05:35 +0000

    Clean up initial test suite and format all adaptnlp and fastapi modules with flake8 manually

--- tests/test_question_answering.py
+++ tests/test_question_answering.py
@@ -3,3 +3,4 @@ from adaptnlp import EasyQuestionAnswering
 
 def test_question_answering():
     qa_model = EasyQuestionAnswering()
+    qa_model.predict_qa(query="Test", context="Test", n_best_size=1)
--- tests/test_sequence_classifier.py
+++ tests/test_sequence_classifier.py
@@ -3,3 +3,4 @@ from adaptnlp import EasySequenceClassifier
 
 def test_easy_sequence_classifier():
     classifier = EasySequenceClassifier()
+    classifier.tag_text(text="test", model_name_or_path="en-sentiment")
