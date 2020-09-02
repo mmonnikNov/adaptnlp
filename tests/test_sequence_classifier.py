c6cfbefe33cea9ac2e9ed6a28f6e59d3de188512
Andrew Chang <aychang995@gmial.com>
2020-09-02 04:33:47 -0400

    Use transformers sequence classification for test

--- tests/test_sequence_classifier.py
+++ tests/test_sequence_classifier.py
@@ -3,4 +3,4 @@ from adaptnlp import EasySequenceClassifier
 
 def test_easy_sequence_classifier():
     classifier = EasySequenceClassifier()
-    classifier.tag_text(text="test", model_name_or_path="en-sentiment")
+    classifier.tag_text(text="test", model_name_or_path="nlptown/bert-base-multilingual-uncased-sentiment")
